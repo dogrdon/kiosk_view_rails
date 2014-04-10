ea45ce683d393dcb2b46daf5d86da05be9b8c251
droquo <drew.s.gordon@gmail.com>
2014-04-10 21:58:34 +0000

    should not let in TIFF files

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ActiveRecord::Base
 
-	has_attached_file :thumb, :styles => { :medium => "200x200>", :thumb => "90x90>" }, :defualt_url => "/images/:style/nada.png"
+	has_attached_file :thumb, :styles => { :medium => "200x200>", :thumb => "90x90>" }, :defualt_url => "/public/images/nada.png"
 	validates_attachment_content_type :thumb, :content_type => /\Aimage\/.*\Z/
+
+	validates :title, presence: true,
+								length: {minimum: 2}
 
 
 end
