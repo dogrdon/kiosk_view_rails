312e5957aedfe0c2a2efa767181aa12261383ec2
droquo <drew.s.gordon@gmail.com>
2014-04-11 20:04:47 +0000

    delete pages, what is up with this ghost page record - having a strug with missing images in paperclip.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,15 @@ class PagesController < ApplicationController
 		redirect_to book_path(@book)
 	end
 
+	def destroy
+		@book = Book.find(params[:book_id])
+		@page = @book.pages.find(params[:id])
+
+		@page.destroy
+
+		redirect_to book_path(@book)
+	end
+
 	private
 		def page_params
 			params.require(:page).permit(:file, :page_num, :comment)
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
 
-	has_many :pages
+	has_many :pages, dependent: :destroy
 
 	has_attached_file :thumb, :styles => { :medium => "200x200>", :thumb => "90x90>" }, :defualt_url => "/public/images/nada.png"
 	validates_attachment_content_type :thumb, :content_type => /\Aimage\/.*\Z/
