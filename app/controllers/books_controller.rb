995d90958df47ce8ddcc0f400bb517c7d2a9ea2e
droquo <drew.s.gordon@gmail.com>
2014-04-11 06:06:54 +0000

    add editing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,20 @@ class BooksController < ApplicationController
 		@books = Book.all
 	end
 
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+
+		if @book.update(book_params)
+			redirect_to @book
+		else
+			render 'edit'
+		end
+	end
+
 
 	private
 		def book_params
