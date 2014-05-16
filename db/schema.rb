8f745a736692918f4fda8c5a6e804c3bca6f0032
droquo <drew.s.gordon@gmail.com>
2014-05-16 21:28:06 +0000

    add admin attr to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516203018) do
+ActiveRecord::Schema.define(version: 20140516212520) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140516203018) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
