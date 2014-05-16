a270da40053f06890f3650ff08c6444978e54198
droquo <drew.s.gordon@gmail.com>
2014-05-16 19:18:45 +0000

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+
+  match '/new_admin', to: 'users#new',  via: 'get'  
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
