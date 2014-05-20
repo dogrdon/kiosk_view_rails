238a4ee95382317d3075e767510f4b795af3beb7
droquo <drew.s.gordon@gmail.com>
2014-05-20 18:08:56 +0000

    user token generator

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516212520) do
+ActiveRecord::Schema.define(version: 20140520151615) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -50,8 +50,10 @@ ActiveRecord::Schema.define(version: 20140516212520) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
